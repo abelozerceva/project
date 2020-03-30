ab7736e4cd8a603d13c12c00fe0725102c0d3caf
belozercevaa <belozerceva.a@yandex.ru>
2020-03-30 22:08:53 +0300

    implemented: flexible arrangement of elements on the page, button with a drop-down menu

--- src/index.js
+++ src/index.js
@@ -1,14 +1,48 @@
 'use strict'
 
 import './main.scss';
-var Masonry = require('masonry-layout');
 
-var container = document.querySelector('#masonry-grid');
-var msnry = new Masonry( container, {
-  // options
-  columnWidth: '.elem',
-  itemSelector: '.elem',
-  // horizontalOrder: true,
-  fitWidth: true,
+function resizeGridItem(item){
+	let grid = document.getElementsByClassName("elements")[0];
+	let rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+	let rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+	let rowSpan = Math.ceil((item.getBoundingClientRect().height + rowGap)/(rowHeight + rowGap)); 
+	item.style.gridRowEnd = "span " + rowSpan;
+}
+  
+function resizeAllGridItems(){
+	let allItems = document.getElementsByClassName("elem");
+	for(let x = 0; x < allItems.length; x++){
+		resizeGridItem(allItems[x]);
+	}
+	if (window.innerWidth > 450) {
+		$('header .menu ')[0].style.display = 'flex';
+		$('header .menu-btn')[0].style.display = 'none';
+		$('header .menu').removeClass('hidden');
+	}
+	else {
+		$('header .menu ')[0].style.display = 'none';
+		$('header .menu-btn')[0].style.display = 'block';
+		$('header .menu').addClass('hidden');
+	}
+	
+}
+  
+// function resizeInstance(instance){
+// 	let item = instance.elements[0];
+// 	resizeGridItem(item);
+// }
+  
+window.onload = resizeAllGridItems();
+window.addEventListener("resize", resizeAllGridItems);
+
+// let allItems = document.getElementsByClassName("elem");
+// for(let x = 0; x < allItems.length; x++){
+// 	imagesLoaded(allItems[x], resizeInstance);
+// }
+
+// menu-btn click
+$('.menu-btn').on('click', () => {
+	$('header .menu').slideToggle('medium');
+	$('header .menu').toggleClass('active');
 });
-
